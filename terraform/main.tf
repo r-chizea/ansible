9919bb70077941388f698fb035b42a35794793d3
Robert Chizea <chizear@icloud.com>
2023-05-17 11:16:15 +0100

    adding security groups to allow ssh

--- terraform/main.tf
+++ terraform/main.tf
@@ -14,4 +14,5 @@ resource "aws_instance" "server" {
   ami = "ami-09744628bed84e434"
   instance_type = "t2.micro"
   key_name = "may23"
+  vpc_security_group_ids = [ "sg-08f4fcf84c89e9658", "sg-0b3e6e5107916b871" ]
 }
