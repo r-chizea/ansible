32f29725666e7354fa542bb4a8341efbfd7dcb02
Robert Chizea <chizear@icloud.com>
2023-05-17 15:36:35 +0100

    final fix (hopefully)

--- terraform/lab4&5/main.tf
+++ terraform/lab4&5/main.tf
@@ -16,7 +16,7 @@ resource "aws_instance" "lab4" {
   key_name = var.my_key
   subnet_id = aws_subnet.lab4_subnet_1.id
   associate_public_ip_address = true
-  security_groups = [ aws_security_group.allow_ssh ]
+  security_groups = [ aws_security_group.allow_ssh.id ]
   tags = {
     key = "project"
     value = "lab_4"
