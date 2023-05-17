7e1e483f04230ff9cb8ba04af0870197339c6abd
Robert Chizea <chizear@icloud.com>
2023-05-17 16:02:07 +0100

    pls.work.im begging.

--- terraform/lab4&5/main.tf
+++ terraform/lab4&5/main.tf
@@ -81,7 +81,7 @@ resource "aws_route_table" "lab4_rt" {
   vpc_id = aws_vpc.lab4_vpc.id
 
   route {
-    cidr_block = "10.0.1.0/24"
+    cidr_block = "0.0.0.0/0"
     gateway_id = aws_internet_gateway.lab4_gw.id
   }
 
