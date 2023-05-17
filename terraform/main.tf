5b10a6e595b15f1a7de2269b99b2db004b58955a
Robert Chizea <chizear@icloud.com>
2023-05-17 12:01:30 +0100

    added bucket + output

--- terraform/main.tf
+++ terraform/main.tf
@@ -16,3 +16,23 @@ resource "aws_instance" "server" {
   key_name = "may23"
   vpc_security_group_ids = [ "sg-08f4fcf84c89e9658", "sg-0b3e6e5107916b871" ]
 }
+
+resource "aws_s3_bucket" "test" {
+  bucket = "tf-bucket-rc"
+
+  tags = {
+    Name = "terraform test bucket"
+  }
+}
+
+output "PublicIP" {
+  value = aws_instance.server.public_ip
+}
+
+output "subnet" {
+  value = aws_instance.server.subnet_id
+}
+
+output "bucket stuff" {
+  value = aws_s3_bucket.test.arn
+}
